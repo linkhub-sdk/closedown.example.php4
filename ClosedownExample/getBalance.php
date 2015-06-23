be661ac590ef1beb887db38c82f6dddf47e38fdf
JohnJeong123 <frenchofkiss@gmail.com>
2015-06-23 14:54:27 +0900

    Init

---
+++ ClosedownExample/getBalance.php
@@ -0,0 +1,41 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" href="../Example.css" media="screen" />
+		<title>휴폐업조회 SDK PHP 4.X Example.</title>
+	</head>
+<?php
+	include 'common.php';
+	// 잔여포인트 조회
+
+	$remainPoint = $closedownChecker->GetBalance();
+
+	if(is_a($remainPoint, 'ClosedownException')){
+		$code = $remainPoint->code;
+		$message = $remainPoint->message;
+	}
+?>
+	<body>
+		<div id="content">
+			<p class="heading1">Response</p>
+			<br/>
+			<fieldset class="fieldset1">
+				<legend>잔여포인트 확인</legend>
+				<ul>
+					<?
+						if(isset($remainPoint)) { 
+					?>
+							<li>잔여포인트 : <? echo $remainPoint ?></li>
+					<?
+						} else {
+					?>
+							<li>Response.code : <? echo $code ?> </li>
+							<li>Response.message : <? echo $message ?></li>
+					<?
+						}
+					?>		
+				</ul>
+			</fieldset>
+		 </div>
+	</body>
+</html>
---
+++ README.md
@@ -0,0 +1,7 @@
+closedon.sdk.example.php4
+==============================
+
+휴폐업조회 SDK Example for PHP 4
+####Requirements
++ php curl module
++ openssl 1.0.1g+ (don't forget about heartbleed.)
